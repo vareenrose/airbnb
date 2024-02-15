7e8789730d0d764506d9410bc452f6146d61e04b
Vareen Agunda <vareen.agunda@turnkeyafrica.com>
2024-02-15 09:47:55 +0300

    fix bug

--- api/index.js
+++ api/index.js
@@ -219,4 +219,4 @@ app.get('/bookings', async (req, res) => {
 })
 
 const port = process.env.PORT || 4000;
-app.listen(port, ".0.0.0.0", () => console.log('server listening on port ' + port));
+app.listen(port, "0.0.0.0", () => console.log('server listening on port ' + port));
